42e957dae42e2afeac3e20e2ed193cbb92e35b3b
Tony McCrae <tony.mccrae.freelance@guardian.co.uk>
2019-02-18 20:18:29 +0000

    Elastic config moves from Guardian properties to Play config. Drop Elastic1 discovery. Drop isDev -  dev has to provide a config just like everyone else.

--- thrall/app/lib/ThrallConfig.scala
+++ thrall/app/lib/ThrallConfig.scala
@@ -2,13 +2,24 @@ package lib
 
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder
 import com.gu.mediaservice.lib.config.CommonConfig
-import com.gu.mediaservice.lib.discovery.EC2
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 import play.api.{Configuration, Logger}
 
 class ThrallConfig(override val configuration: Configuration) extends CommonConfig {
-  Logger.info("Configurating thrall")
+  Logger.info("Configuring thrall")
+
+  lazy val elasticsearchPort: Option[Int] = configuration.getOptional[Int]("es.port")
+  lazy val elasticsearchCluster: Option[String] = configuration.getOptional[String]("es.cluster")
+
+  lazy val elasticsearch6Host: Option[String] = configuration.getOptional[String]("es6.host")
+  lazy val elasticsearch6Port: Option[Int] = configuration.getOptional[Int]("es6.port")
+  lazy val elasticsearch6Cluster: Option[String] = configuration.getOptional[String]("es6.cluster")
+  lazy val elasticsearch6Shards: Option[Int] = configuration.getOptional[Int](("es6.shards"))
+  lazy val elasticsearch6Replicas: Option[Int] = configuration.getOptional[Int](("es6.replicas"))
+
+  lazy val elasticsearchHost: Option[String] = configuration.getOptional[String]("es.host")
+
   private lazy val ec2Client = withAWSCredentials(AmazonEC2ClientBuilder.standard()).build()
 
   final override lazy val appName = "thrall"
@@ -20,32 +31,6 @@ class ThrallConfig(override val configuration: Configuration) extends CommonConf
   lazy val writeAlias: String = properties.getOrElse("es.index.aliases.write", configuration.get[String]("es.index.aliases.write"))
 
   lazy val thumbnailBucket: String = properties("s3.thumb.bucket")
-
-  lazy val elasticsearchHost: Option[String] = Some {
-    if (isDev)
-      properties.getOrElse("es.host", "localhost")
-    else
-      EC2.findElasticsearchHostByTags(ec2Client, Map(
-        "Stage" -> Seq(stage),
-        "Stack" -> Seq(elasticsearchStack),
-        "App" -> Seq(elasticsearchApp)
-      ))
-  }
-
-  lazy val elasticsearchPort: Option[Int] = properties.get("es.port").map(_.toInt)
-  lazy val elasticsearchCluster: Option[String] = properties.get("es.cluster")
-
-  lazy val elasticsearch6Host: Option[String] =  {
-    if (isDev)
-      Some(properties.getOrElse("es6.host", "localhost"))
-    else
-      properties.get("es6.host")
-  }
-
-  lazy val elasticsearch6Port: Option[Int] = properties.get("es6.port").map(_.toInt)
-  lazy val elasticsearch6Cluster: Option[String] = properties.get("es6.cluster")
-  lazy val elasticsearch6Shards = Some(if (isDev) 1 else properties("es6.shards").toInt)
-  lazy val elasticsearch6Replicas = Some(if (isDev) 0 else properties("es6.replicas").toInt)
 
   lazy val healthyMessageRate: Int = properties("sqs.message.min.frequency").toInt
 
