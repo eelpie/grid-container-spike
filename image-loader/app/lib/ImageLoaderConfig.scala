ec08d3df35415b8fbbe3da88dd940de870986846
Tony McCrae <tony.mccrae.freelance@guardian.co.uk>
2019-02-26 13:08:25 +0000

    Image loader starts with Play config file.

--- image-loader/app/lib/ImageLoaderConfig.scala
+++ image-loader/app/lib/ImageLoaderConfig.scala
@@ -9,13 +9,14 @@ class ImageLoaderConfig(override val configuration: Configuration) extends Commo
 
   final override lazy val appName = "image-loader"
 
-  val topicArn: String = properties("sns.topic.arn")
+  val topicArn: String = configuration.get[String]("sns.topic.arn")
 
-  val imageBucket: String = properties("s3.image.bucket")
+  val imageBucket: String = configuration.get[String]("s3.image.bucket")
 
-  val thumbnailBucket: String = properties("s3.thumb.bucket")
+  val thumbnailBucket: String = configuration.get[String]("s3.thumb.bucket")
 
-  val tempDir: File = new File(properties.getOrElse("upload.tmp.dir", "/tmp"))
+  val configuredTempDir = configuration.getOptional[String]("upload.tmp.dir")
+  val tempDir: File = new File(configuredTempDir.getOrElse("/tmp")) // TODO Api call rather than /tmp
 
   val thumbWidth: Int = 256
   val thumbQuality: Double = 85d // out of 100
