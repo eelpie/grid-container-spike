631b1438e7313ccdfe9b6f17e31dcb5c7981ecc2
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-03-04 22:00:03 +0000

    Refactor; rename to indiciate duration rather than count metric type.

--- media-api/app/lib/MediaApiMetrics.scala
+++ media-api/app/lib/MediaApiMetrics.scala
@@ -6,7 +6,7 @@ import com.gu.mediaservice.lib.metrics.CloudWatchMetrics
 
 class MediaApiMetrics(config: MediaApiConfig) extends CloudWatchMetrics(s"${config.stage}/MediaApi", config) {
 
-  val searchQueries = new TimeMetric("ElasticSearch")
+  val searchQueryDuration = new TimeMetric("ElasticSearch")
 
   def searchTypeDimension(value: String): Dimension =
     new Dimension().withName("SearchType").withValue(value)
