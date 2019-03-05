b1b5149133c6ac2f8fc6093e2792db0be5467756
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-03-05 08:52:43 +0000

    Metadata editor's instance of MessageConsumer missed the change of the message consumer metric from latancy to message count. Apply it now.

--- metadata-editor/app/lib/MetadataMessageConsumer.scala
+++ metadata-editor/app/lib/MetadataMessageConsumer.scala
@@ -6,7 +6,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 class MetadataMessageConsumer(config: EditsConfig, metadataEditorMetrics: MetadataEditorMetrics, store: EditsStore) extends MessageConsumer(
-  config.queueUrl, config.awsEndpoint, config, metadataEditorMetrics.processingLatency) {
+  config.queueUrl, config.awsEndpoint, config, metadataEditorMetrics.snsMessage) {
 
   override def chooseProcessor(message: UpdateMessage): Option[UpdateMessage => Future[Any]] =
     PartialFunction.condOpt(message.subject) {
