eb4c3c596fee1d29f674d6b11030ce6fda8f3326
Tony McCrae <tony@eelpieconsulting.co.uk>
2019-03-05 09:20:40 +0000

    Document private usage metrics; the interface is via the methods.

--- usage/app/lib/UsageMetrics.scala
+++ usage/app/lib/UsageMetrics.scala
@@ -6,6 +6,6 @@ class UsageMetrics(config: UsageConfig) extends CloudWatchMetrics(s"${config.sta
   def incrementUpdated = updates.increment().run
   def incrementErrors = errors.increment().run
 
-  val updates = new CountMetric("UsageUpdates")
-  val errors = new CountMetric("UsageUpdateErrors")
+  private val updates = new CountMetric("UsageUpdates")
+  private val errors = new CountMetric("UsageUpdateErrors")
 }
